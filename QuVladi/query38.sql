38d096f2034be831bac8eb3cb2aebd15bd1e2fcb
Oliver Suarez <oliver.suareztinoco@gmail.com>
2026-06-23 10:23:00 -0500

    cambios

--- QuVladi/query38.sql
+++ QuVladi/query38.sql
@@ -10,14 +10,18 @@ as
 begin
 set nocount on
 
-;with tmp001_sep(t,r)as(
-    select*from(values('|','~'))t(SepCamp,SepReg)
-)
-select stuff((select top 50 r,
-Proy_Id, t, Proy_Nombre, t, Proy_Descripcion, t, Proy_ArchivoLogo
+-- ;with tmp001_sep(t,r)as(
+--     select*from(values('|','~'))t(SepCamp,SepReg)
+-- )
+-- select stuff((select top 50 r,
+-- Proy_Id, t, Proy_Nombre, t, Proy_Descripcion, t, Proy_ArchivoLogo
+-- from dbo.a10_proyectos
+-- for xml path, type).value('.','varchar(max)'),1,1,'') data
+-- from tmp001_sep
+
+select top 10
+Proy_Id, Proy_Nombre, Proy_Descripcion, Proy_ArchivoLogo
 from dbo.a10_proyectos
-for xml path, type).value('.','varchar(max)'),1,1,'') data
-from tmp001_sep
 
 select count(1) totalReg from dbo.a10_proyectos
 
